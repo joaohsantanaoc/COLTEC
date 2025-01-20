1a8c65bad072d7e6519b7fe6d2b14be4e61d2be5
João Henrique Santana Oliveira Campos <mistrjoaosantana@gmail.com>
2025-01-20 13:25:00 -0300

    primeiros passos

---
+++ Revisão C/Introdução/primeiro_programa.c
@@ -0,0 +1,23 @@
+/*
+19/01/2025
+Revisão Linguagem C: Introdução
+Primeiro programa em C
+João Henrique Santana Oliveira Campos
+Turma 304 - Automação Industrial
+Professor Márcio Fantini
+*/
+
+// Header files presentes no código:
+#include <stdio.h>
+
+// Constantes presentes no código:
+#define SUCESSO 0
+
+// Função principal:
+int main(int argc, char ** argv){
+
+    printf("Hello World!!!\n");
+
+    // Se tudo deu certo é porque chegou até aqui!!!
+    return SUCESSO;
+}
